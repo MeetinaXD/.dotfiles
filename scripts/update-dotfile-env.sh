fb8047ef02cea0ac925e6e125e0a7b4e67bff949
MeetinaXD <capitaycity@gmail.com>
2025-04-02 21:53:54 +0800

    update brew check and install

--- scripts/update-dotfile-env.sh
+++ scripts/update-dotfile-env.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+if ! command -v brew&>/dev/null; then
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
 /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME pull &&\
 brew bundle
 
