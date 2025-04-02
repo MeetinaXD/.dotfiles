4c3b6a44a0a19533dda5010e5228c22d07de2488
MeetinaXD <capitaycity@gmail.com>
2025-04-02 21:58:07 +0800

    update brew check and install

--- scripts/update-dotfile-env.sh
+++ scripts/update-dotfile-env.sh
@@ -2,6 +2,8 @@
 
 if ! command -v brew&>/dev/null; then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> $HOME/.zprofile
+  eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
 /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME pull &&\
 brew bundle
